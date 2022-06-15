8bd9a0ae47cb2af712e44a4b1dfc2ddae52dfe70
zakiah <zakiahzulkefli91@gmail.com>
2022-06-16 00:16:00 +0800

    remove clientId and secretId

--- server side/googleSignInConfig.php
+++ server side/googleSignInConfig.php
@@ -6,9 +6,9 @@
 	
 	//zakiah12062022: all these secretId, clientSecret & redirect URL must be same with https://console.cloud.google.com/apis/credentials?project=test11062022
 	//we also must registered the redirectUri
-	$google_client->setClientId("764810202688-lejbpe6trp7c8dtse3cah51jnad7pfo5.apps.googleusercontent.com");
+	$google_client->setClientId("");
 	
-	$google_client->setClientSecret("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");
+	$google_client->setClientSecret("");
 	
 	$google_client->setRedirectUri("http://localhost/mywampvirtualhost/googleSignIn/createUserIdWithGoogleAcc.php");
 	
